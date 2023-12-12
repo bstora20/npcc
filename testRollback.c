60b5378e136da3c8016547ff2b3954d1f5cbffbd
Blanche Stora <stora1@ruby963.llnl.gov>
2023-12-12 14:46:46 -0800

    test has working function

--- testRollback.c
+++ testRollback.c
@@ -10,34 +10,38 @@ static int in = 0;
 static uintptr_t last_random_number;
 
 volatile uint64_t prngState[2];
-
-static inline uintptr_t getRandomPre()
+static inline uintptr_t getRandomPre(int rollback)
 {
 	// https://en.wikipedia.org/wiki/Xorshift#xorshift.2B
 	uint64_t x = prngState[0];
 	const uint64_t y = prngState[1];
-	prngState[0] = y;
+	prngState[0] = prngState[0] * !rollback + rollback * y;
 	x ^= x << 23;
 	const uint64_t z = x ^ y ^ (x >> 17) ^ (y >> 26);
-	prngState[1] = z;
+	prngState[1] = prngState[1] * !rollback + rollback * z;
 	return (uintptr_t)(z + y);
 }
 void precalculate_random_numbers() {
     for (int i = 0; i < BUFFER_SIZE; i++) {
-        buffer[i] = getRandomPre();
+        buffer[i] = getRandomPre(1);
     }
 }
-static inline uintptr_t getRandom() {
-    uintptr_t num = buffer[in];
-    last_random_number = num;  // Store the last random number
-	buffer[in] = getRandomPre();  // Generate a new random number and add it to the buffer
-    in = (in + 1) % BUFFER_SIZE;  // Wrap around to 0 when index reaches BUFFER_SIZE
-    return num;
+volatile uint64_t prngStateOG[2];
+static inline uintptr_t getRandom()
+{
+    // https://en.wikipedia.org/wiki/Xorshift#xorshift.2B
+    uint64_t x = prngStateOG[0];
+    const uint64_t y = prngStateOG[1];
+    prngStateOG[0] = y;
+    x ^= x << 23;
+    const uint64_t z = x ^ y ^ (x >> 17) ^ (y >> 26);
+    prngStateOG[1] = z;
+    return (uintptr_t)(z + y);
 }
 static inline uintptr_t getRandomRollback(uintptr_t rollback) {
     uintptr_t num = buffer[in];
     last_random_number = num;  // Store the last random number
-    uintptr_t new_num = getRandomPre();
+    uintptr_t new_num = getRandomPre(rollback);
     buffer[in] = (new_num & -rollback) | (num & ~-rollback);  // Generate a new random number only if rollback is not zero
     in = ((in + 1) & -rollback) | (in & ~-rollback);  // Roll back if rollback is zero
     return num;
@@ -49,12 +53,18 @@ int main() {
     prngState[0] = time(NULL);
     prngState[1] = 0xDEADBEEF;
 
+    prngStateOG[0] = time(NULL);
+    prngStateOG[1] = 0xDEADBEEF;
+
     // Precalculate random numbers
     precalculate_random_numbers();
 
     // Get a random number without rollback
     uintptr_t num1 = getRandomRollback(1);
     printf("Random number without rollback: %lu\n", num1);
+
+    uintptr_t numOG = getRandom();
+    printf("Random number OG: %lu\n", numOG);
 
     // Get a random number with rollback
     uintptr_t num2 = getRandomRollback(0);
@@ -70,6 +80,8 @@ int main() {
     // Get another random number without rollback
     uintptr_t num3 = getRandomRollback(1);
     printf("Another random number without rollback: %lu\n", num3);
+    uintptr_t numOG2 = getRandom();
+    printf("Random number OG2: %lu\n", numOG2);
 
     // Check if the new random number is different from the previous numbers
     if (num3 != num1 && num3 != num2) {
